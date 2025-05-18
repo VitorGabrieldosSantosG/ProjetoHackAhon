0dcd402fe62e0d6754537e84587bf92815cc7b06
Malv741 <malvcronosgmail.com>
2025-05-18 08:35:16 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -75,55 +75,36 @@ botaoBuscar.onclick = () => {
                 const lon = parseFloat(data[0].lon);
                 map.setView([lat, lon], 16);
 
-                // Busca estacionamentos públicos próximos ao endereço
-                const queryPublicos = `
-                [out:json][timeout:25];
-                (
-                  node["amenity"="parking"](around:1000,${lat},${lon});
-                  way["amenity"="parking"](around:1000,${lat},${lon});
-                  relation["amenity"="parking"](around:1000,${lat},${lon});
-                );
-                out center;
-                `;
+                // Lista fixa dos 5 estacionamentos particulares
+                const estacionamentosParticulares = [
+                    {
+                        nome: "Ouro Verde",
+                        preco: "R$ 8,00",
+                        vagas: 15
+                    },
+                    {
+                        nome: "Central Park",
+                        preco: "R$ 10,00",
+                        vagas: 12
+                    },
+                    {
+                        nome: "Esta Center",
+                        preco: "R$ 7,00",
+                        vagas: 18
+                    },
+                    {
+                        nome: "Com Freedom III",
+                        preco: "R$ 9,00",
+                        vagas: 10
+                    },
+                    {
+                        nome: "SS Park",
+                        preco: "R$ 6,00",
+                        vagas: 20
+                    }
+                ];
 
-                fetch('https://overpass-api.de/api/interpreter', {
-                    method: 'POST',
-                    body: queryPublicos
-                })
-                .then(response => response.json())
-                .then(data => {
-                    // Remove os marcadores anteriores
-                    estacionamentosData = [];
-                    map.eachLayer(layer => {
-                        if (layer instanceof L.Marker && layer.options.icon === iconeEstacionamento) {
-                            map.removeLayer(layer);
-                        }
-                    });
-
-                    // Adiciona os novos estacionamentos públicos ao mapa
-                    data.elements.forEach(element => {
-                        const lat = element.lat || element.center?.lat;
-                        const lon = element.lon || element.center?.lon;
-                        if (lat && lon) {
-                            const nome = element.tags?.name || "Estacionamento Público";
-                            estacionamentosData.push({
-                                nome,
-                                lat,
-                                lon,
-                                preco: "Gratuito",
-                                vagas: Math.floor(Math.random() * 20 + 5)
-                            });
-
-                            L.marker([lat, lon], { icon: iconeEstacionamento })
-                                .addTo(map)
-                                .bindPopup(`<strong>${nome}</strong><br>💰 <strong>Preço:</strong> Gratuito<br>📌 <strong>Vagas disponíveis:</strong> ${Math.floor(Math.random() * 20 + 5)}`);
-                        }
-                    });
-
-                    // Exibe os estacionamentos no modal
-                    mostrarModal(estacionamentosData);
-                })
-                .catch(err => console.error("Erro ao buscar estacionamentos públicos:", err));
+                mostrarModal(estacionamentosParticulares);
             } else {
                 alert("Endereço não encontrado. Tente novamente.");
             }
@@ -133,6 +114,7 @@ botaoBuscar.onclick = () => {
 
 function mostrarModal(lista) {
     listaEstacionamentos.innerHTML = '';
+
     lista.forEach(e => {
         const li = document.createElement('li');
         li.innerHTML = `
